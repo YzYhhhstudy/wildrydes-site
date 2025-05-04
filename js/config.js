8d8893b471e11631eaf8d5eeab50177e793930a1
Zoey Young <zoey@Zoeys-MacBook-Pro.local>
2025-05-04 19:33:43 -0400

    new-conf

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_EuKmnwCjB', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '5osjsud49i5kda756hbpdf2kg0', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_OehiIJgMi', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '28b1v51d34fj58jpnisu5i5luu', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
